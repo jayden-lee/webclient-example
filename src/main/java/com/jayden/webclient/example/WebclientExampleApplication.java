a61be7c181f59e94cb94ffaba0a25fadba2d2e82
jayden-lee <gwanggi.lee@gmail.com>
2020-12-27 17:38:18 +0900

    Init project

---
+++ src/main/java/com/jayden/webclient/example/WebclientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jayden.webclient.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclientExampleApplication.class, args);
+    }
+
+}
